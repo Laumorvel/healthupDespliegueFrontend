9b9ff9f0808537193939994675a574ae54c8ce82
lauramolez <lauramorelez@gmail.com>
2022-03-06 20:27:48 +0100

    rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl: 'https://healthupheroku.herokuapp.com/',
+  baseUrl: 'https://healthupheroku.herokuapp.com',
   production: false
 };
 
