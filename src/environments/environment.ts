f8c4e24d381132f0d65960c37fa7b2bbf81e1c29
Laura Moreno <73167874+Laumorvel@users.noreply.github.com>
2022-03-07 21:37:33 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl: 'https://healthupmysql.herokuapp.com/',
+  baseUrl: 'https://healthupmysql.herokuapp.com',
   production: false
 };
 
