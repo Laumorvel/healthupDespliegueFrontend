0d5628c74d056bda2ba3bd1bfbff2b5da8d27fc2
lauramolez <lauramorelez@gmail.com>
2022-03-10 08:57:18 +0100

    accesibilidad añadida

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  fontSize = 20;
+  dislexia = false;
+
+
+  dislexiaFriendly(){
+    if(!this.dislexia){
+      document.getElementsByTagName('h2')[0].style.fontFamily  = `OpenDyslexic3Regular`;
+      document.getElementsByTagName('h2')[1].style.fontFamily  = `OpenDyslexic3Regular`;
+      document.getElementsByTagName('strong')[0].style.fontFamily  = `OpenDyslexic3Regular`;
+      document.getElementsByTagName('strong')[1].style.fontFamily  = `OpenDyslexic3Regular`;
+      this.dislexia = true;
+    }else{
+      document.getElementsByTagName('h2')[0].style.fontFamily  = `Quicksand, sans-serif`;
+      document.getElementsByTagName('h2')[1].style.fontFamily  = `Quicksand, sans-serif`;
+      document.getElementsByTagName('strong')[0].style.fontFamily  = `Quicksand, sans-serif`;
+      document.getElementsByTagName('strong')[1].style.fontFamily  = `Quicksand, sans-serif`;
+      this.dislexia = false;
+    }
+  }
+
+  changeFont(operator:any){
+    operator === '+' ? this.fontSize++ : this.fontSize--;
+    document.getElementsByTagName('h2')[0].style.fontSize  = `${this.fontSize}px`;
+    document.getElementsByTagName('h2')[1].style.fontSize  = `${this.fontSize}px`;
+  }
 }
